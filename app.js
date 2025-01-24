e39d13af7927f42ae8cde29433b638c4a75cf9f9
MarcosChaves81 <marcosd.a.chaves3@gmail.com>
2025-01-24 19:35:38 -0300

    crea funciones

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaDeAmigos = []
+
+let maximoAmigos = 6
+function guardarNombres(listaDeAmigo) {
+    if (maximoAmigos <=6){
+        {
+            let nombre = document.getElementById('amigo').value;
+            if (nombre.trim() !== '') {
+                listaDeAmigos.push(nombre);
+                document.getElementById('amigo').value = '';
+                maximoAmigos++
+            } else {
+                alert('Por favor ingresa un nombre valido.'); 
+        return
+            }
+        }
+    }           
+}
+
+function generarAmigosecreto(listaDeAmigos) {
+    return listaDeAmigos[Math.floor(Math.random)*listaDeAmigos.length]
+
+    return
+}
+
+function mostrarNombres(listaDeAmigo){
+
+
+    return
+}
