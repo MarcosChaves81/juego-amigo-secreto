202471bf85f935a1a5330379f8f2753798e9f636
MarcosChaves81 <marcosd.a.chaves3@gmail.com>
2025-01-25 17:27:23 -0300

    refinar el codigo

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listaDeAmigos = []
+let listaDeAmigos = [];
+let maximoAmigos = 0;
 
-let maximoAmigos = 0
-function agregarAmigos(listaDeAmigo) {
-    if (maximoAmigos <=6){
-        {
-            let nombre = document.getElementById('amigo').value;
-            if (nombre.trim() !== '') {
-                listaDeAmigos.push(nombre);
-                document.getElementById('amigo').value = '';
-                maximoAmigos++
-            } else {
-                alert('Por favor ingresa un nombre valido.'); 
-        return
-            }
+function agregarAmigo() {
+    if (maximoAmigos < 5) {
+        let nombre = document.getElementById('amigo').value;
+        if (nombre !== '') {
+            listaDeAmigos.push(nombre);
+            document.getElementById('amigo').value = '';
+            maximoAmigos++;
+            console.log(listaDeAmigos);
+        } else {
+            alert('Por favor, ingresa un nombre válido.'); 
+            return;
         }
-    }else{
-        alert('Se ha alcanzado el número máximo de entradas.')
+    } else {
+        alert('Se ha alcanzado el número máximo de entradas.');
     }           
 }
 
-function generarAmigosecreto(listaDeAmigos) {
-    return listaDeAmigos[Math.floor(Math.random)*listaDeAmigos.length]
 
-    return
+function sortearAmigo() {
+    let ganador = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
+    alert(ganador);
+    return;
 }
 
 function mostrarNombres(listaDeAmigo){
